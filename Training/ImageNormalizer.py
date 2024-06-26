cc5703e1ced58e75c1b5b56070f0c4915a67b31b
David <122132914+EnderBoy9217@users.noreply.github.com>
2024-06-26 09:09:38 -0400

    Update ImageNormalizer to output a single tensor

--- Training/ImageNormalizer.py
+++ Training/ImageNormalizer.py
@@ -2,7 +2,8 @@ import torch
 from PIL import Image
 import numpy as np
 
-def imageTensors(image_path):
+def imageToTensor(image_path) -> torch.Tensor:
+    # Open the image file
     img = Image.open(image_path)
     
     # Check if the image size is 1000x1000
@@ -10,21 +11,10 @@ def imageTensors(image_path):
         raise ValueError("Image dimensions must be 1000x1000.")
     
     # Convert the image data to numpy arrays
-    rgba_data = np.array(img.convert('RGBA'))
+    rgbData = np.array(img.convert('RGB'))
     
-    # Separate the RGBA channels
-    rChannel = rgba_data[:, :, 0]
-    gChannel = rgba_data[:, :, 1]
-    bChannel = rgba_data[:, :, 2]
-    #aChannel = rgba_data[:, :, 3]
+    normalizedRgbData = rgbData[:, :, :] / 255.0
 
-    rNormal = rChannel / 255.0
-    gNormal = gChannel / 255.0
-    bNormal = bChannel / 255.0
+    rgbTensor = torch.from_numpy(normalizedRgbData).float()
     
-    # Convert the numpy arrays to PyTorch tensors
-    rTensor = torch.from_numpy(rNormal).float()
-    gTensor = torch.from_numpy(gNormal).float()
-    bTensor = torch.from_numpy(bNormal).float()
-    
-    return rTensor, gTensor, bTensor
+    return rgbTensor
