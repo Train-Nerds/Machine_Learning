a0fe31b6327d3a58c6fede178f70b7c58eabe1e0
Profetes <bbsaw1108@gmail.com>
2024-07-01 12:41:47 -0400

    Added blue channel for large bodies of water
    Also happened to restructure the program to allow for looping of the program (not working yet!)

--- terrainGeneration.py
+++ terrainGeneration.py
@@ -6,80 +6,13 @@ import heapq
 import random
 from scipy.ndimage import gaussian_filter
 
-def rgb_norm(world):
-    world_min = np.min(world)
-    world_max = np.max(world)
-    norm = lambda x: (x-world_min/(world_max - world_min))*255
-    return np.vectorize(norm)
-
-def prep_world(world):
-    norm = rgb_norm(world)
-    world = norm(world)
-    return world
-
-shape = (1024,1024)
-scale = 200
-octaves = 6
-persistence = 0.5
-lacunarity = 2.0
-seed = np.random.randint(20,200)
-print(str(seed))
-
-world = np.zeros(shape)
-for i in range(shape[0]):
-    for j in range(shape[1]):
-        world[i][j] = noise.pnoise2(i/scale, 
-                                    j/scale, 
-                                    octaves=octaves, 
-                                    persistence=persistence, 
-                                    lacunarity=lacunarity, 
-                                    repeatx=1024, 
-                                    repeaty=1024, 
-                                    base=seed)
-        
-center_x, center_y = shape[1] // 2, shape[0] // 2
-circle_grad = np.zeros_like(world)
-
-for y in range(world.shape[0]):
-    for x in range(world.shape[1]):
-        distx = abs(x - center_x)
-        disty = abs(y - center_y)
-        dist = math.sqrt(distx*distx + disty*disty)
-        circle_grad[y][x] = dist
-
-# get it between -1 and 1
-max_grad = np.max(circle_grad)
-circle_grad = circle_grad / max_grad
-circle_grad -= 0.8
-circle_grad *= 1.2
-circle_grad = -circle_grad
-
-world_noise = np.zeros_like(world)
-
-for i in range(shape[0]):
-    for j in range(shape[1]):
-        #if circle_grad[i][j] > 0:
-        world_noise[i][j] = (world[i][j] * circle_grad[i][j])
-print("Terrain Generated!")
-
-lightblue = [0,191,255]
-blue = [65,105,225]
-green = [34,139,34]
-darkgreen = [0,100,0]
-sandy = [210,180,140]
-beach = [238, 214, 175]
-snow = [255, 250, 250]
-mountain = [139, 137, 137]
-
-threshold = 50
-
 def add_color2(informationMap):
     color_world = np.zeros(world.shape+(3,))
     for i in range(shape[0]):
         for j in range(shape[1]):
-            if informationMap.getpixel((i, j))[0] < threshold + 55 or informationMap.getpixel((i, j))[2] > 0:
+            if informationMap.getpixel((i, j))[0] < threshold + 52 or informationMap.getpixel((i, j))[2] > 0:
                 color_world[i][j] = blue
-            elif informationMap.getpixel((i, j))[0] < threshold + 57:
+            elif informationMap.getpixel((i, j))[0] < threshold + 55:
                 color_world[i][j] = beach
             elif informationMap.getpixel((i, j))[0] < threshold + 60:
                 color_world[i][j] = sandy
@@ -180,6 +113,11 @@ def generate_rivers(terrain, num_rivers, river_threshold):
         for x, y in path:
             rivers[y, x] = 255  # Mark the river in the blue channel
             #print("River grew!")
+            
+    for i in range(len(terrain)):
+        for j in range(len(terrain[i])):
+            if(terrain[i][j] < -0.07):
+                rivers[i, j] = 255
     
     return rivers
 
@@ -190,9 +128,9 @@ def generate_population(terrain, rivers, num_clusters, growth_steps, growth_prob
     
     i = 0
     for _ in range(num_clusters):
-        print("Creating population cluster")
+        #print("Creating population cluster")
         while True:
-            print("Choosing a cluster")
+            #print("Choosing a cluster")
             # Randomly choose a center for the cluster
             center_x = random.randint(0, width - 1)
             center_y = random.randint(0, height - 1)
@@ -201,20 +139,20 @@ def generate_population(terrain, rivers, num_clusters, growth_steps, growth_prob
             # print(str(i) + ". Terrain Coord: " + str(terrain[center_y, center_x]))
             # Check if the chosen point is suitable for a city (near but not on a river and mid-elevation)
             if terrain[center_y, center_x] > 0 and terrain[center_y, center_x] < 0.1:
-                print("Calculating city river proximity...")
+                #print("Calculating city river proximity...")
                 river_proximity = np.min([np.sqrt((center_x - rx)**2 + (center_y - ry)**2) for ry, rx in zip(*np.where(rivers == 255))])
-                print("River proximity calculated!")
+                #print("River proximity calculated!")
                 if 5 < river_proximity < 50 or 0.02 > random.random():  # The city is between 5 and 50 pixels away from a river
                     break
         
         # Grow the city from the chosen starting point
         grow_city(population, center_x, center_y, terrain, rivers, growth_steps, growth_probability)
     
-    print("Population generated!")
+    #print("Population generated!")
     return gaussian_filter(population, sigma=4)
 
 def grow_city(city, start_x, start_y, terrain, rivers, growth_steps, growth_probability):
-    print("Grow city,..")
+    #print("Grow city,..")
     height, width = terrain.shape
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Four possible directions: up, down, left, right
     
@@ -239,25 +177,95 @@ def grow_city(city, start_x, start_y, terrain, rivers, growth_steps, growth_prob
                 nx, ny = x + dx, y + dy
                 if valid_location(nx, ny):
                     city[ny, nx] = 255
-                    print("City grew!")
+                    #print("City grew!")
                     frontier.append((nx, ny))
         
         # Remove the point from the frontier to prevent regrowth
         frontier.remove((x, y))
 
+def rgb_norm(world):
+    world_min = np.min(world)
+    world_max = np.max(world)
+    norm = lambda x: (x-world_min/(world_max - world_min))*255
+    return np.vectorize(norm)
 
-     
-rivers = generate_rivers(world_noise.copy(), 20, .1)
-population = generate_population(world_noise.copy(), rivers, 3, 100, 0.9).astype(np.uint8)
+def prep_world(world):
+    norm = rgb_norm(world)
+    world = norm(world)
+    return world
 
-heightmap_grad = prepareChannel(prep_world(world_noise)).astype(np.uint8)
-heightmap = Image.fromarray(heightmap_grad)
-rivermap = Image.fromarray(rivers.astype(np.uint8))
-rivermap.save("rivermap.png")
-populationMap = Image.fromarray(population.astype(np.uint8))
-populationMap.save("populationmap.png")
-heightmap = heightmap.convert("L")
-informationMap = Image.merge("RGB", (heightmap, populationMap, rivermap))
-informationMap.save("heightmapR.png")
-informationMap.show()
-Image.fromarray(add_color2(informationMap).astype(np.uint8)).show()
+i = 0
+
+while(i < 100):
+    shape = (1024,1024)
+    scale = 200
+    octaves = 6
+    persistence = 0.5
+    lacunarity = 2.0
+    seed = np.random.randint(20,200)
+    print(str(seed) +  " Iteration: " + str(i))
+
+    world = np.zeros(shape)
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            world[i][j] = noise.pnoise2(i/scale, 
+                                        j/scale, 
+                                        octaves=octaves, 
+                                        persistence=persistence, 
+                                        lacunarity=lacunarity, 
+                                        repeatx=1024, 
+                                        repeaty=1024, 
+                                        base=seed)
+            
+    center_x, center_y = shape[1] // 2, shape[0] // 2
+    circle_grad = np.zeros_like(world)
+
+    for y in range(world.shape[0]):
+        for x in range(world.shape[1]):
+            distx = abs(x - center_x)
+            disty = abs(y - center_y)
+            dist = math.sqrt(distx*distx + disty*disty)
+            circle_grad[y][x] = dist
+
+    # get it between -1 and 1
+    max_grad = np.max(circle_grad)
+    circle_grad = circle_grad / max_grad
+    circle_grad -= 0.8
+    circle_grad *= 1.2
+    circle_grad = -circle_grad
+
+    world_noise = np.zeros_like(world)
+
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            #if circle_grad[i][j] > 0:
+            world_noise[i][j] = (world[i][j] * circle_grad[i][j])
+    print("Terrain Generated!")
+
+    lightblue = [0,191,255]
+    blue = [65,105,225]
+    green = [34,139,34]
+    darkgreen = [0,100,0]
+    sandy = [210,180,140]
+    beach = [238, 214, 175]
+    snow = [255, 250, 250]
+    mountain = [139, 137, 137]
+
+    threshold = 50
+
+    rivers = generate_rivers(world_noise.copy(), 20, .1)
+    population = generate_population(world_noise.copy(), rivers, 3, 100, 0.9).astype(np.uint8)
+
+    heightmap_grad = prepareChannel(prep_world(world_noise)).astype(np.uint8)
+    heightmap = Image.fromarray(heightmap_grad)
+    rivermap = Image.fromarray(rivers.astype(np.uint8))
+    #rivermap.save("rivermap.png")
+    populationMap = Image.fromarray(population.astype(np.uint8))
+    #populationMap.save("populationmap.png")
+    heightmap = heightmap.convert("L")
+    informationMap = Image.merge("RGB", (heightmap, populationMap, rivermap))
+    informationMap.save("terrainHeightmap" + str(i) + ".png")
+    #informationMap.show()
+    #Image.fromarray(add_color2(informationMap).astype(np.uint8)).show()
+    
+    i += 1
