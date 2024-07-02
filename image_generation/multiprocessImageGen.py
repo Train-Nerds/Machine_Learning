471731268c5e9167ad629ab0eea3a6abf93f2af2
Brayden Lee <braydenphiliplee@gmail.com>
2024-07-01 22:16:25 -0400

    Minor multiprocessing configurability improvement.

--- image_generation/multiprocessImageGen.py
+++ image_generation/multiprocessImageGen.py
@@ -1,18 +1,21 @@
 from multiprocessing import Process
 import os
 
-threads = 10
+THREADS = 10 # Don't use green threads, use real cores (hyperthreading counts as real cores).
+IMAGES_PER_THREAD = 20
 
 def work(t):
     print(f"Started work on {t}.")
-    for _ in range(20): os.system('python3 terrainGeneration.py')
+    for _ in range(IMAGES_PER_THREAD): os.system('python3 terrainGeneration.py')
     print(f"Finished work on {t}.")
 
 if __name__ == '__main__':
     processes = []
-    for t in range(threads):
+    print(f"Amount to generate: {THREADS * IMAGES_PER_THREAD}.")
+    for t in range(THREADS):
         p = Process(target=work, args=(t,))
         p.start()
         processes.append(p)
     for p in processes:
         p.join()
+    print("Done.")
